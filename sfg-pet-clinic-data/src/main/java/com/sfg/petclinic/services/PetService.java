829599d922858fb95691d1fb42b7b418433f0c09
Gaurav Singh <grvsgh7@gmail.com>
2023-09-02 19:18:04 +0530

    added services. Closes #5

---
+++ sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package com.sfg.petclinic.services;
+
+import java.util.Set;
+
+import com.sfg.petclinic.model.Pet;
+
+public interface PetService {
+	Pet findById(Long id);
+
+	Pet save(Pet pet);
+
+	Set<Pet> findAll();
+}
---
+++ sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package com.sfg.petclinic.services;
+
+import java.util.Set;
+
+import com.sfg.petclinic.model.Vet;
+
+public interface VetService {
+	Vet findById(Long id);
+
+	Vet save(Vet vet);
+
+	Set<Vet> findAll();
+}
