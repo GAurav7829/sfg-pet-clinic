34d5b35645a08fc0ee8ffa3cfc185b6c9195a22e
Gaurav Singh <grvsgh7@gmail.com>
2023-09-03 21:59:07 +0530

    managed id property auto generated. Closes #23

--- sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/services/map/AbstractMapService.java
+++ sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package com.sfg.petclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+import com.sfg.petclinic.model.BaseEntity;
 
-	protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+	protected Map<Long, T> map = new HashMap<>();
 
 	Set<T> findAll() {
 		return new HashSet<>(map.values());
@@ -17,8 +20,14 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 
-	T save(ID id, T object) {
-		map.put(id, object);
+	T save(T object) {
+		if(null!=object) {
+			if(null==object.getId())
+				object.setId(getNextId());
+			map.put(object.getId(), object);
+		}else {
+			throw new RuntimeException("Object can not be null");
+		}
 		return object;
 	}
 
@@ -28,6 +37,16 @@ public abstract class AbstractMapService<T, ID> {
 
 	void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+
+	private Long getNextId() {
+		Long nextId = null;
+		try {
+			nextId = Collections.max(map.keySet()) + 1L;
+		}catch(Exception ex) {
+			nextId = 1L;
+		}
+		return nextId;
 	}
 
 }
