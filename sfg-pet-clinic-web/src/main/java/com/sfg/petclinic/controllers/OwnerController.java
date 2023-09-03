941da99cfdfef9c04c4c0552ea8d8eea01f40fcf
Gaurav Singh <grvsgh7@gmail.com>
2023-09-03 21:17:29 +0530

    list owner on index page. Closes #16

--- sfg-pet-clinic-web/src/main/java/com/sfg/petclinic/controllers/OwnerController.java
+++ sfg-pet-clinic-web/src/main/java/com/sfg/petclinic/controllers/OwnerController.java
@@ -1,14 +1,29 @@
 package com.sfg.petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sfg.petclinic.model.Owner;
+import com.sfg.petclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({ "", "/", "/index", "/index.html" })
-	public String listOwners() {
+	public String listOwners(Model model) {
+		Set<Owner> owners = ownerService.findAll();
+		model.addAttribute("owners", owners);
 		return "owner/index";
 	}
 }
