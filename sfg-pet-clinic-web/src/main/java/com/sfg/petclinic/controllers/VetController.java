0ff9825ae09a1b2ffd556a049f90b47717311398
Gaurav Singh <grvsgh7@gmail.com>
2023-09-03 21:21:02 +0530

    list vet on index page. Closes #15

--- sfg-pet-clinic-web/src/main/java/com/sfg/petclinic/controllers/VetController.java
+++ sfg-pet-clinic-web/src/main/java/com/sfg/petclinic/controllers/VetController.java
@@ -1,13 +1,28 @@
 package com.sfg.petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sfg.petclinic.model.Vet;
+import com.sfg.petclinic.services.VetService;
 
 @Controller
 public class VetController {
+	
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
 
 	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html" })
-	public String listVets() {
+	public String listVets(Model model) {
+		Set<Vet> vets = vetService.findAll();
+		model.addAttribute("vets", vets);
 		return "vets/index";
 	}
 }
