0765ce9d9ea9653d4dff188fd2fb46a35a3d651a
Gaurav Singh <grvsgh7@gmail.com>
2023-09-08 19:05:21 +0530

    created Vets Entity. closes #36

--- sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/model/Vet.java
+++ sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/model/Vet.java
@@ -3,8 +3,19 @@ package com.sfg.petclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
-	
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality> specialities = new HashSet<>();
 
 	public Set<Speciality> getSpecialities() {
