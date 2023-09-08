def9b0beb5672b236b4064522948d15d4912f4d1
Gaurav Singh <grvsgh7@gmail.com>
2023-09-08 18:46:00 +0530

    Implemented BaseEntity. Closes #34

--- sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/model/BaseEntity.java
+++ sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/model/BaseEntity.java
@@ -2,8 +2,17 @@ package com.sfg.petclinic.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	private static final long serialVersionUID = -1006807498524942332L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
