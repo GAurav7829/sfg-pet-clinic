71351caff0a5309baefcc309008b132f24b33479
Gaurav Singh <grvsgh7@gmail.com>
2023-09-08 19:11:53 +0530

    created Visit entity. Closes #37

--- sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/model/Pet.java
+++ sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package com.sfg.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +26,8 @@ public class Pet extends BaseEntity {
 	private Owner owner;
 	@Column(name = "birth_date")
 	private LocalDate dateOfBirth;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 
 	public String getName() {
 		return name;
@@ -53,6 +59,14 @@ public class Pet extends BaseEntity {
 
 	public void setDateOfBirth(LocalDate dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 }
