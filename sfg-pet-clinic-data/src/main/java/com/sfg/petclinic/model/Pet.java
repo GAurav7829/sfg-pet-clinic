14c5f23932eff25c2d1de8bb8ff67f4875553305
Gaurav Singh <grvsgh7@gmail.com>
2023-09-08 18:57:40 +0530

    converted Owners to JPA Entity. Closes @35

--- sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/model/Pet.java
+++ sfg-pet-clinic-data/src/main/java/com/sfg/petclinic/model/Pet.java
@@ -2,11 +2,25 @@ package com.sfg.petclinic.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 	
+	@Column(name = "name")
 	private String name;
+	@ManyToOne
+	@JoinColumn(name = "type_id")
 	private PetType petType;
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
 	private Owner owner;
+	@Column(name = "birth_date")
 	private LocalDate dateOfBirth;
 
 	public String getName() {
